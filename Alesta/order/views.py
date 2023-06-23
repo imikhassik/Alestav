83e1b4f53ed1cbbcd97fb880da5efcb791935974
Ilya Mikhasik <imikhassik@gmail.com>
2023-06-23 18:53:36 +0300

    Fix naive datetime

--- Alesta/order/views.py
+++ Alesta/order/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from django.db.models import Sum
-from drf_spectacular.utils import extend_schema
+from django.utils import timezone
+from drf_spectacular.utils import extend_schema, OpenApiExample
 from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework.decorators import action
@@ -35,9 +38,14 @@ class ServiceViewset(mixins.CreateModelMixin,
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    @extend_schema(examples=[OpenApiExample('Example 1',
+                                            summary='Общая сумма услуг по счетам за период',
+                                            value="{'price__sum': 1000}")])
     @action(detail=False, url_path='total_service_sum/(?P<from_date>[^/.]+)/(?P<to_date>[^/.]+)')
-    def total_service_sum(self, request, from_date, to_date):
-        queryset = Service.objects.filter(created_date__range=(from_date, to_date))
+    def total_service_sum(self, request, from_date, to_date) -> Response:
+        queryset = Service.objects.filter(created_date__range=
+                                          (timezone.make_aware(datetime.strptime(from_date, '%Y-%m-%d')),
+                                           timezone.make_aware(datetime.strptime(to_date, '%Y-%m-%d'))))
         total_sum = queryset.aggregate(Sum('price'))
         return Response(total_sum)
 
